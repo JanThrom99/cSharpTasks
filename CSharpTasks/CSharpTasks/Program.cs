cf1e1d977be6892587413c4723fa9bd4f0be124a
Jan Throm <jan.throm@tecalliance.net>
2022-09-15 11:26:05 +0200

    task 1 completed #1

---
+++ CSharpTasks/CSharpTasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1 
+            int a = 3;
+            int b = 4;
+            int sum = a + b;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}

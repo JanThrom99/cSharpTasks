5a36ff2c92ff70e6d9d94d3ccd84c8e9636e90ef
Jan Throm <jan.throm@tecalliance.net>
2022-09-19 15:09:26 +0200

    introduction Interfaces and inheritance

---
+++ CSharpTasks/CSharpTasks/Kreis.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpTasks
+{
+    public class Kreis : IGeometry
+    {
+        public float Radius { get; set; }
+
+        public float Umfang()
+        {
+            return 2 * (float)Math.PI * Radius;
+        }
+
+        public float Flaeche()
+        {
+            return (float)Math.PI * Radius * Radius;
+        }
+
+        public Kreis()
+        {
+            
+        }
+
+        public Kreis(float radius)
+        {
+            Radius = radius;
+        }
+    }
+}
---
+++ CSharpTasks/CSharpTasks/Vehicle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTasks
+{
+    public abstract class Vehicle
+    {
+        public int Tyres { get; set; }
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int CCM { get; set; }
+        public bool Motorlaeuft { get; private set; }
+        public int Geschwindigkeit { get; private set; }
+
+
+        public void Starten()
+        {
+            if (this.Motorlaeuft)
+            {
+                Console.WriteLine("Mehr als Motor an geht nicht!");
+            }
+            else
+            {
+                this.Motorlaeuft = true;
+                Console.WriteLine("der Motor ist jetzt an ");
+            }
+        }
+
+        public void Beschleunigen()
+        {
+            Geschwindigkeit += 10;
+            Console.WriteLine($"{this.GetType().Name} wurde Beschleunigt auf {Geschwindigkeit} Km/h");
+        }
+
+        public void Bremsen()
+        {
+            if (Geschwindigkeit <=0)
+            {
+                Console.WriteLine($"{this.GetType().Name} steht bereits");
+            }
+            else
+            {
+                Geschwindigkeit -= 10;
+                Console.WriteLine($"{this.GetType().Name} wird gebremst auf {Geschwindigkeit} Km/h");
+            }
+        }
+    }
+}

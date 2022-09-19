a9b7bd3c437a6c4c5f5ef4af7030f2698f9ed3b8
Jan Throm <jan.throm@tecalliance.net>
2022-09-19 15:09:37 +0200

    Revert "introduction Interfaces and inheritance"
    This reverts commit 5a36ff2c92ff70e6d9d94d3ccd84c8e9636e90ef.

---
+++ CSharpTasks/CSharpTasks/House.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CSharpTasks
+{
+    public class House
+    {
+        public float HouseLength { get; set; }
+        public float HouseWidth { get; set; }
+        public int HouseHeight { get; set; }
+        public float PropertyLength { get; set; }
+        public float PropertyWidth { get; set; }
+        public string RoofType { get; set; }
+        public string Color { get; set; }
+        public float Stories { get; set; }
+        public decimal Price { get; set; }
+        public int PropertySize { get; private set; }
+        public int LivingArea { get; private set; }
+
+        public House(float houseLength, int houseHeight, float houseWidth, float propertyLength, float propertyWidth, string roofType, string color, int stories, decimal price )
+        {
+            HouseHeight = houseHeight;
+            HouseLength = houseLength;
+            HouseWidth = houseWidth;
+            PropertyLength = propertyLength;
+            PropertyWidth = propertyLength;
+            RoofType = roofType;
+            Color = color;
+            Stories = stories;
+            Price = price;
+        }
+
+        public void CalculatePropertySize()
+        {
+            PropertySize = Convert.ToInt32(PropertyLength * PropertyWidth);
+            Console.WriteLine(PropertySize);
+        }
+
+        public void CalculateLivingArea()
+        {
+            LivingArea =  Convert.ToInt32(HouseLength * HouseWidth* Stories);
+            Console.WriteLine(LivingArea);
+        }
+
+        public void ChangeColor(string newColor)
+        {
+            Color = newColor;
+            Console.WriteLine($"Das Haus wurde in {newColor} neu gestrichen");
+        }
+
+        public void PrintProperties()
+        {
+            Console.WriteLine($"hier hast du die Eigenschaften deines Hauses: " +
+                                     $"\n Hauslänge: {HouseLength} " +
+                                     $"\n Hausbreite: {HouseWidth} " +
+                                     $"\n Hausbreite: {HouseLength} " +
+                                     $"\n PropertyLength: {PropertyLength}" +
+                                     $"\n PropertyWidth: {PropertyWidth}" +
+                                     $"\n RoofType: {RoofType}" +
+                                     $"\n Color: {Color}" +
+                                     $"\n Stories: {Stories}" +
+                                     $"\n Price: {Price}");
+        }
+    }
+}
--- CSharpTasks/CSharpTasks/Program.cs
+++ CSharpTasks/CSharpTasks/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CSharpTasks
 {
@@ -7,44 +6,12 @@ namespace CSharpTasks
     {
         public static void Main(string[] args)
         {
-            Dreieck d = new Dreieck(4,5,10);
-            //d.Seite1 = 4;
-            //d.Seite2 = 5;
-            //d.Seite3 = 10;
+            House foo = new House(16, 8, 10, 45, 50, "reetgedeckt", "grün", 3, 350000);
 
-            Kreis k = new Kreis(5);
-            //k.Radius = 5;
-            
-            Rechteck r = new Rechteck(10,25);
-            //r.Seite1 = 10;
-            //r.Seite2 = 25;
-            
-            List<IGeometry> geometries = new List<IGeometry>();
-            geometries.Add(d);
-            geometries.Add(k);
-            geometries.Add(r);
-
-            foreach (var g in geometries)
-            {
-                Console.WriteLine($"Mein {g.GetType().Name} hat eine Fläche von {g.Flaeche()} und Umfang von {g.Umfang()}");
-            }
-
-            List<Vehicle> fahrzeuge = new List<Vehicle>();
-            fahrzeuge.Add(new Motorcycle("BMW","1000RR",2490,"rot",2));
-            fahrzeuge.Add(new Car(201,"Mercedes","190E",1996,"blau",4));
-
-            foreach (var fahrzeug in fahrzeuge)
-            {
-                fahrzeug.Starten();
-                fahrzeug.Beschleunigen();
-                fahrzeug.Beschleunigen();
-                fahrzeug.Beschleunigen();
-                fahrzeug.Beschleunigen();
-                fahrzeug.Bremsen();
-                Console.WriteLine($"{fahrzeug.GetType().Name}, {fahrzeug.Make}");
-            }
-
-
+            foo.CalculateLivingArea();
+            foo.CalculatePropertySize();
+            foo.ChangeColor("rot");
+            foo.PrintProperties();
             Console.ReadKey();
         }
     }
